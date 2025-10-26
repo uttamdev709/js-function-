7509af8c32c3422fcf90eb921ad460a35a4e09a0
uttamdev709 < GitHub >
2025-10-26 21:00:50 +0600

    add all file

---
+++ exam.js
@@ -0,0 +1,15 @@
+Functions
+function addNumbers( a,b ) {
+    return a+b;
+}
+
+let a= 5;
+let b = 7;
+console.log(" The sum is",addNumbers(a,b))
+
+
+let name = "Uttam";
+let age = 22;
+let isStudent = true;
+console.log("My name is "+ name +  ",I am " + age + " years old"+ ",and student status is "+isStudent );
+
